6fffd1ea80c0935487af26c31540af3ef85d8736
Bryan Hanner <Bryan Hanner>
2015-11-21 12:37:04 -0800

    I added code to the methods getPacket and parsePacketData.

--- prototypeCode/roverCode/prototype/wirelessCommunication.ino
+++ prototypeCode/roverCode/prototype/wirelessCommunication.ino
@@ -1,4 +1,5 @@
 // All Functions for Wireless Communication //
+#include <SPI.h> 
 #include "Arduino.h"
 #include "config.h"
 #include <Ethernet.h>
@@ -8,6 +9,9 @@
 EthernetUDP Udp;
 byte MAC_ADDRESS[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress IP(192, 168, 1, 4);
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+int x = 0;
+int y = 0;
 
 void initializeWirelessCommunication()
 {
@@ -18,11 +22,56 @@ void initializeWirelessCommunication()
 
 void getPacket()
 {
+    int packetSize = Udp.parsePacket();
+    //Serial.println(x);
+    //Serial.println(y);
+    if(packetSize)
+    {
+        /*int actual = 0;
+        Serial.print("Received packet of size ");
+        Serial.println(packetSize);
+        Serial.print("From ");
+        IPAddress remote = Udp.remoteIP();
+        for (int i =0; i < 4; i++)
+        {
+          actual = (remote[i], HEX);
+          Serial.print(actual);
+          if (i < 3)
+          {
+              Serial.print(".");
+          }
+        }
+        Serial.print(", port ");
+        Serial.println(Udp.remotePort());*/
     
+        // read the packet into packetBufffer
+        Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
+        //Serial.println("Contents:");
+        //Serial.println(packetBuffer);
+    }
 }
 
 void parsePacketData()
 {
-    
+    boolean isFirstHalf = true;
+    for (int i = 0; i < UDP_TX_PACKET_MAX_SIZE; i++)
+    {
+        if (packetBuffer[i] != '\0')
+        {
+            char current = packetBuffer[i];
+            if (current == ',')
+            {
+                isFirstHalf = false;
+            }
+            else if (isFirstHalf)
+            {
+                x = x*10 + (packetBuffer[i] - 48);
+            }
+            else
+            {
+                y = y*10 + (packetBuffer[i] - 48);
+            }
+        }
+    }
 }
 
