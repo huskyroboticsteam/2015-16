efb99dab573bde71b89b7103a2a5201cd43e30f0
Elizabeth Moore <emoore2500@gmail.com>
2015-11-21 11:16:36 -0800

    Completed writeToMotors

--- prototypeCode/roverCode/prototype/steering.ino
+++ prototypeCode/roverCode/prototype/steering.ino
@@ -3,14 +3,23 @@
 #include "config.h"
 #include <Ethernet.h>
 #include <EthernetUdp.h>
+#include <Servo.h>
+#define DELAY 100
+Servo frontRight;
+Servo frontLeft;
+Servo backRight;
+Servo backLeft;
 
 void calculateMotorSpeeds()
 {
     
 }
 
-void writeToMotors()
+void writeToMotors(int frontRightVal, int frontLeftVal, int backRightVal, int backLeftVal)
 {
-    
+    frontRight.writeMicroseconds(map(frontRightVal, -100, 100, 1000, 2000));
+    frontLeft.writeMicroseconds(map(frontLeftVal, -100, 100, 1000, 2000));
+    backRight.writeMicroseconds(map(backRightVal, -100, 100, 1000, 2000));
+    backLeft.writeMicroseconds(map(backLeftVal, -100, 100, 1000, 2000));
+    delay(DELAY);
 }
-
