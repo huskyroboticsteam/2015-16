d93fe5246a356d11e926f185e80e889ff456696d
Justin Warner <jkwdives@hotmail.com>
2016-02-13 12:08:47 -0800

    Revert "Deleted old arduino files"
    This reverts commit e53787456e4e4254768810d79296a232701cc8d8.

---
+++ prototypeCode/roverCode/prototype/prototype.ino
@@ -0,0 +1,44 @@
+#include "Arduino.h"
+#include "config.h"
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <Servo.h>
+
+EthernetUDP Udp;
+byte MAC_ADDRESS[] = {0x90, 0xA2, 0xDA, 0x00, 0x3D, 0x8B};
+IPAddress IP(192, 168, 1, 51);
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+bool networkStatus = true;
+bool hasIP = false;
+unsigned long timeLastPacket; // to be set to millis() in main code
+
+int inputAngle  = 10;
+int speed  = 128;
+int motorRatio = 1;
+int currentAngle = 0;
+
+Servo frontRight;
+Servo frontLeft;
+Servo backRight;
+Servo backLeft;
+int frontRightVal = 0;
+int frontLeftVal = 0;
+int backRightVal = 0;
+int backLeftVal = 0;
+
+void setup()
+{
+    initializeWirelessCommunication();
+    initializeSteeringSystem();
+}
+
+void loop()
+{
+   // receiveDX6iData();
+    receiveWirelessData();
+    readCurrentAngle();
+    calculateMotorSpeeds();
+    writeToMotors();
+    timeoutCheck();
+}
+
