6a7173d9abd745857792098dd38f1ea049dfff3d
Bryan Hanner <Bryan Hanner>
2016-02-27 13:40:22 -0800

    sends ph and humidity data over udp

--- roverCode/sensorBoard/sensors/sensors/sensors.ino
+++ roverCode/sensorBoard/sensors/sensors/sensors.ino
@@ -16,6 +16,9 @@ IPAddress ipComputer(192, 168, 1, 5);
 float intercept = 13.720;
 float slope = -3.838;
 unsigned int localPort = 8888;      // local port to listen on
+bool phDone = false;
+bool humidityDone = false;
+char sensorData[9];
 
 // An EthernetUDP instance to let us send and receive packets over UDP
 
@@ -25,40 +28,55 @@ void setup() {
   Ethernet.begin(mac,ipArduino);
   Udp.begin(localPort);
   Wire.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
+    phLoop();
+    if (phDone) {
+        humidityLoop();
+        if (humidityDone) {
+            sensorData[strlen(sensorData)] = '\0';
+            Udp.beginPacket(ipComputer, localPort);
+            Udp.write(sensorData);
+            Udp.endPacket();
+            phDone = false;
+            humidityDone = false;
+        }
+    }
+    delay(100);
+}
 
-  //calculate pH from original analog reading
-  float count = analogRead(A0);
-  float voltage = count / 1023 * 5.0;// convert from count to raw voltage
-  float pH = intercept + voltage * slope; //converts voltage to sensor reading
-  Udp.beginPacket(ipComputer, localPort);
+void phLoop() {
+    
+    //calculate pH from original analog reading
+    float count = analogRead(A0);
+    float voltage = count / 1023 * 5.0;// convert from count to raw voltage
+    float pH = intercept + voltage * slope; //converts voltage to sensor reading
   
-  //get digits of pH
-  int phTen = (int)pH%100/10;
-  int phOne = (int)pH%10;
-  int phTenth = (int)(pH*10)%10;
-  int phHundredth = (int)(pH*100)%10;
+    //get digits of pH
+    int phTen = (int)pH%100/10;
+    int phOne = (int)pH%10;
+    int phTenth = (int)(pH*10)%10;
+    int phHundredth = (int)(pH*100)%10;sensorData[0] = (char)phTen+48;
+    sensorData[1] = (char)phOne+48;
+    sensorData[2] = (char)phTenth+48;
+    sensorData[3] = (char)phHundredth+48;
+    sensorData[4] = ',';
+    phDone = true;
+}
 
-  //get digits of humidity
-  float humidity = analogRead(A1);
-  int humidityHundred = (int)humidity/100;
-  int humidityTen = (int)humidity%100/10;
-  int humidityOne = (int)humidity%10;
+void humidityLoop() {
+    float humidity = analogRead(A2);
 
-  //put pH in array and send over udp
-  char sensorData[9];
-  sensorData[0] = (char)phTen+48;
-  sensorData[1] = (char)phOne+48;
-  sensorData[2] = (char)phTenth+48;
-  sensorData[3] = (char)phHundredth+48;
-  sensorData[4] = ',';
-  sensorData[5] = (char)humidityHundred;
-  sensorData[6] = (char)humidityTen;
-  sensorData[7] = (char)humidityOne;
-  sensorData[8] = '\0';
-  Udp.write(sensorData);
-  Udp.endPacket();
-  delay(100);
+    //get digits of humidity
+    int humidityHundred = (int)humidity/100;
+    int humidityTen = (int)humidity%100/10;
+    int humidityOne = (int)humidity%10;
+  
+    //put pH in array and send over udp
+    sensorData[5] = (char)humidityHundred+48;
+    sensorData[6] = (char)humidityTen+48;
+    sensorData[7] = (char)humidityOne+48;
+    humidityDone = true;
 }
