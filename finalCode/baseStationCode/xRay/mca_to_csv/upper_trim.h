93b853105091189a384e6547d966543b955ffcaf
Aakash Sethi <aakash.k.sethi@gmail.com>
2016-05-31 13:38:33 -0700

    Reorganized, updated README

---
+++ finalCode/baseStationCode/xRay/mca_to_csv/upper_trim.h
@@ -0,0 +1,18 @@
+/*
+ *  upper_trim.h
+ *  APS1IDanalysis
+ *
+ *  Created by W. T. Elam on 4/8/11.
+ *
+ */
+
+#ifndef upper_trim_h
+#define upper_trim_h
+
+#include <string>
+
+//		convert to upper case and remove trailing blanks
+
+std::string upper_trim( const std::string inStr );
+
+#endif
